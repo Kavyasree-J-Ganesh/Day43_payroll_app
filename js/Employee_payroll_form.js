f045048d193e56f3b160097d9dd08e14e8bba9dc
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-20 01:06:50 +0530

    [Kavyasree j ganesh] Employee payroll app

--- js/Employee_payroll_form.js
+++ js/Employee_payroll_form.js
@@ -1,7 +1,7 @@
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
-salary.addEventListener('input', function() {
+salary.addEventListener('change', function() {
     output.textContent = salary.value;
 });
 
@@ -19,4 +19,25 @@ function save(event){
     empDate.startDate = ("0" + event.target.Month.value).slice(-2) + "-" + ( "0" + event.target.Day.value).slice(-2) + "-"  + event.target.year.value;
     localStorage.setItem("personObj", empDate.toString())
     alert(empDate.toString())
+}
+
+function reset(){
+    setValues("name", "");
+    setValues("note", "");
+    setValues("salary", 40000)
+    setValues("day", "1");
+    setValues("month", "01");
+    setValues("year", "2023");
+    unSelectSelected("[name=profile")
+    unSelectSelected("[name=gender")
+    unSelectSelected("[name=department")
+}
+
+const setValues = (id, value)=>{
+    document.getElementById(id).textContent = value
+}
+
+const unSelectSelected = (prop)=>{
+    let allItems = document.querySelectorAll(prop);
+    allItems.forEach(item=> item.checked = false)
 }
