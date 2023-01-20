f7b5320e44529eac3e450715ad7c7968e7e7a2ac
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-21 00:57:43 +0530

    [Kavyasree j ganesh] Employee payroll app home page

--- js/Employee_payroll_home.js
+++ js/Employee_payroll_home.js
@@ -1,55 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event)=>{
-   storeEmpListToLocatStorage();
-   createInnerHtml();
-})
-
-
-const createInnerHtml = () => {
-    const employeeList = getEmpPayrollListFromLocalStorage();
-   const headerHtml = `
-   <tr>
-   <th></th>
-   <th>Name</th>
-   <th>Gender</th>
-   <th>Department</th>
-   <th>Salary</th>
-   <th>Start Date</th>
-   <th>Action</th>
-   </tr>`
-
-   innerHtml = `${headerHtml}`
-
-   for( const employee of employeeList){
-    innerHtml = `${innerHtml} 
-    <tr>
-         <td>
-             <img src="${employee.profileUrl}" alt="" class="profile">
-        </td>
-        <td>${employee.name}</td>
-        <td>${employee.gender}</td>
-        <td>
-           ${getDeptHtml(employee.departMent)}
-        <td>${employee.salary}</td>
-        <td>${employee.startDate}</td>
-        <td>
-            <img id="${employee.id}" alt="Delete" src="./assets/icons/delete-black-18dp.svg">
-           <img id="${employee.id}"  alt="Edit" src="./assets/icons/create-black-18dp.svg">
-        </td>
-    </tr>`
-   }
-
-   document.querySelector("#display").innerHTML = innerHtml;
-}
-
-const getDeptHtml = (departments)=>{
-    let deptHtml = '';
-    for(let dept of departments){
-        deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
-    }
-}
-
-const storeEmpListToLocatStorage = ()=>{
-    localStorage.setItem("empPayrollList", JSON.stringify([
+   if(!localStorage.getItem("empPayrollList")){
+    storeEmpListToLocatStorage([
         {
           "name": "mohit kumar new",
           "gender": "male",
@@ -98,9 +49,73 @@ const storeEmpListToLocatStorage = ()=>{
           "id": 1604589731061,
           "profileUrl": "../assets/profile-images/Ellipse -3.png"
         }
-      ]))
+       ]);
+   }
+   createInnerHtml();
+})
+
+let employeeList = [];
+const createInnerHtml = () => {
+   employeeList = getEmpPayrollListFromLocalStorage();
+   const headerHtml = `
+   <tr>
+   <th></th>
+   <th>Name</th>
+   <th>Gender</th>
+   <th>Department</th>
+   <th>Salary</th>
+   <th>Start Date</th>
+   <th>Action</th>
+   </tr>`
+
+   innerHtml = `${headerHtml}`
+
+   for( const employee of employeeList){
+    innerHtml = `${innerHtml} 
+    <tr>
+         <td>
+             <img src="${employee.profileUrl}" alt="" class="profile">
+        </td>
+        <td>${employee.name}</td>
+        <td>${employee.gender}</td>
+        <td>
+           ${getDeptHtml(employee.departMent)}
+        <td>${employee.salary}</td>
+        <td>${employee.startDate}</td>
+        <td>
+            <img id="${employee.id}" onclick="deletePerson(${employee.id})" alt="Delete" src="./assets/icons/delete-black-18dp.svg">
+           <img id="${employee.id}"  alt="Edit" src="./assets/icons/create-black-18dp.svg">
+        </td>
+    </tr>`
+   }
+
+   document.querySelector("#display").innerHTML = innerHtml;
+}
+
+const getDeptHtml = (departments)=>{
+    let deptHtml = '';
+    for(let dept of departments){
+        deptHtml = `${deptHtml} <div class="dept-label">${dept}</div>`
+    }
+
+    return deptHtml;
+}
+
+const storeEmpListToLocatStorage = (empList)=>{
+        localStorage.setItem("empPayrollList", JSON.stringify(empList)) 
 }
 
 const getEmpPayrollListFromLocalStorage = ()=>{
     return localStorage.getItem("empPayrollList") ? JSON.parse(localStorage.getItem("empPayrollList")) : []
+}
+
+
+const deletePerson = (id) =>{
+    if(employeeList.find(emp=> emp.id === id)){
+        
+        employeeList = employeeList.filter(emp=> emp.id != id);
+
+        storeEmpListToLocatStorage(employeeList)
+        createInnerHtml();
+    }
 }
